53baaa0ccbc15bfd7fa2f16c1971c29cd8b62041
Sourav Kumar Dubey <73059672+souravkumardubey@users.noreply.github.com>
2022-06-09 21:57:03 +0530

    Space optimised

--- MajorityElement.cpp
+++ MajorityElement.cpp
@@ -1,3 +1,7 @@
+Brute Force approach
+TC : O(n)
+SC : O(n) -> map space
+
 #include <bits/stdc++.h> 
 int findMajorityElement(int arr[], int n) {
 	// Write your code here.
@@ -12,5 +16,41 @@ int findMajorityElement(int arr[], int n) {
     }
     
     return -1;
+    
+}
+
+// -------------------------------------------------------------------------------------------------------------------------------------------
+
+Space optimised approach
+TC : O(n)
+SC : O(1)
+
+
+#include <bits/stdc++.h> 
+int findMajorityElement(int arr[], int n) {
+	// Write your code here.
+    
+    int ans = 0 , cnt = 0;
+    
+    for ( int i = 0 ; i < n ; i++ ) {
+        
+        if ( cnt == 0 ) ans = arr[i];
+        
+        if ( ans == arr[i] ) cnt += 1;
+        else cnt -= 1;
+        
+    }
+    
+    cnt = 0;
+    
+    for ( int i = 0 ; i < n ; i++ ) {
+        
+        if ( arr[i] == ans ) cnt += 1;
+        
+    }
+    
+    if ( cnt <= n / 2 ) return -1;
+    
+    return ans;
     
 }
